52e5ceb392aacfba99c5438cefcfe4874441547c
jimkoch <jimkoch@gmail.com>
2020-12-31 21:01:39 -0500

    machine_learning_01: Linear model for calculating payments.  To Do: Convert to exponential.

--- machine_learning_01/main.py
+++ machine_learning_01/main.py
@@ -9,24 +9,43 @@
 # n = number of payments.
 
 import tensorflow
-import logging
 
-logger = tensorflow.get_logger()
-logger.setLevel(logging.ERROR)
+parameters = [(100000, 0.005, 360),
+              (150000, 0.005, 360),
+              (200000, 0.005, 360),
+              (100000, 0.025, 360),
+              (400000, 0.020, 240),
+              (450000, 0.020, 240),
+              (20000, 0.045, 60),
+              (45000, 0.035, 60),
+              (90000, 0.025, 60)]
 
-celsius_q = [-40, -10, 0, 8, 15, 22, 38]
-fahrenheit_a = [-40, 14, 32, 46, 59, 72, 100]
+payments = [599.55,
+            899.33,
+            1199.10,
+            2500.34,
+            8069.63,
+            9078.34,
+            969.09,
+            1803.99,
+            2911.81]
 
 def calculate_monthly_payment(principal: float, interest_rate: float, number_of_payments: float) -> float:
-    return principal * (interest_rate * ((1 + interest_rate) ^ number_of_payments) / ((1 + interest_rate) ^ number_of_payments - 1))
+    return principal * (interest_rate * ((1.0 + interest_rate) ** number_of_payments) / ((1.0 + interest_rate) ** number_of_payments - 1.0))
 
 def main():
-
-    for i,c in enumerate(celsius_q):
-        print("{} degrees Celsius = {} degree Fahrenheit".format(c, fahrenheit_a[i]))
+    for i, value in enumerate(parameters):
+        principal = value[0]
+        interest_rate = value[1]
+        number_of_payments = value[2]
+        print("Principal = ${:,}".format(principal))
+        print("Interest Rate = {:.2f}%".format(interest_rate * 100))
+        print("Number of Payments = {}".format(number_of_payments))
+        print("Monthly Payment = ${:,.2f}".format(calculate_monthly_payment(principal, interest_rate, number_of_payments)))
+        print("-----")
 
     # Create a model with a single layer with a single neuron.
-    l0 = tensorflow.keras.layers.Dense(units=1, input_shape=[1])
+    l0 = tensorflow.keras.layers.Dense(units=1, input_shape=[3,])
 
     # Define the order in which the model will run the various layers.
     model = tensorflow.keras.Sequential([l0])
@@ -36,11 +55,11 @@ def main():
                 optimizer=tensorflow.keras.optimizers.Adam(0.1))
 
     # Then use the fit function to train the model.
-    history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+    history = model.fit(parameters, payments, epochs=500, verbose=False)
     print("Finished training the model")
 
     # Use the model to predict a value
-    print(model.predict([100.0]))
+    print(model.predict([(125000, 0.075, 360)]))
 
     # See our layer's weights
     print(l0.get_weights())
